81f43850d8d6b0eaa84804d9ef60b5a65e55a168
nathandickinson32 <nathandickinson32@gmail.com>
2025-04-28 10:26:04 -0400

    finihsed euler problem 1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,9 +7,38 @@
 
 (describe "Euler Problem #1"
 
+  (it "finds the multiples of x of n when x = 5"
+    (should= [] (get-multiples-of-x 0 5))
+    (should= [] (get-multiples-of-x 1 5))
+    (should= [] (get-multiples-of-x 2 5))
+    (should= [] (get-multiples-of-x 3 5))
+    (should= [] (get-multiples-of-x 4 5))
+    (should= [] (get-multiples-of-x 5 5))
+    (should= [5] (get-multiples-of-x 6 5))
+    (should= [5] (get-multiples-of-x 7 5))
+    (should= [5] (get-multiples-of-x 8 5))
+    (should= [5] (get-multiples-of-x 9 5))
+    (should= [5] (get-multiples-of-x 10 5))
+    (should= [5 10] (get-multiples-of-x 11 5))
+    )
+  (it "finds the multiples of x of n when x = 3"
+    (should= [] (get-multiples-of-x 0 3))
+    (should= [] (get-multiples-of-x 1 3))
+    (should= [] (get-multiples-of-x 2 3))
+    (should= [] (get-multiples-of-x 3 3))
+    (should= [3] (get-multiples-of-x 4 3))
+    (should= [3] (get-multiples-of-x 5 3))
+    (should= [3] (get-multiples-of-x 6 3))
+    (should= [3 6] (get-multiples-of-x 7 3))
+    (should= [3 6] (get-multiples-of-x 8 3))
+    (should= [3 6] (get-multiples-of-x 9 3))
+    (should= [3 6 9] (get-multiples-of-x 10 3))
+    )
+
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+    (should= 45 (euler-1 15))
+    (should= 233168 (euler-1 1000)))
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,19 @@
 (ns euler.level1.problem001)
 
+(defn get-multiples-of-x [n x]
+  (filter #(zero? (mod % x)) (range x n)))
+
 (defn euler-1 [n]
+  (reduce +
+          (set
+            (concat
+              (get-multiples-of-x n 3)
+              (get-multiples-of-x n 5))))
+  ;(->> [3 5]
+  ;     (map #(get-multiples-of-x n %))
+  ;     (apply concat)
+  ;     set
+  ;     (reduce +))
   )
+
+

7ed748ba0def92fb8de60a8ebfbabbf66cee0024
nathandickinson32 <nathandickinson32@gmail.com>
2025-05-14 12:15:10 -0400

    solved euler 8

--- src/euler/level1/problem008.clj
+++ src/euler/level1/problem008.clj
@@ -1,1 +1,22 @@
 (ns euler.level1.problem008)
+
+(defn multiply-digits [n]
+  (reduce * (map #(Integer/parseInt (str %)) (str n))))
+
+(defn sliding-substring [digit-as-string substring-length]
+  (map #(apply str %) (partition substring-length 1 digit-as-string)))
+
+(defn max-product [digit-as-string substring-length]
+  (->> (sliding-substring digit-as-string substring-length)
+       (map multiply-digits)
+       (apply max)))
+
+
+; 1000 digit number
+; get the product of first 13 digits
+; move one digit over
+; get product of those 13 digits
+; compare and take larger
+; keep shifting one digit and getting 13 digit product
+; keep comparing and keeping largest product
+; go through 1000 digit number

c858919eb097611e8cf31cb768981fced5e479a4
nathandickinson32 <nathandickinson32@gmail.com>
2025-05-19 18:43:37 -0400

    removed errors in 5

--- src/euler/level1/problem005.clj
+++ src/euler/level1/problem005.clj
@@ -11,24 +11,3 @@
       (greatest-common-denominator first-number second-number)))
   ([range-of-numbers]
    (reduce least-common-multiple 1 range-of-numbers)))
-
-
-(int (dec (gcd 1 2 num)))
-(->> num
-     (gcd 1 2)
-     dec
-     int)
-
-
-(int (dec (lcd num 1 2)))
-
-(-> num
-     (lcd 1 2)
-     dec
-     int)
-
-(int (dec (mel 1 num 2)))
-
-(-> (mel 1 num 2)
-    dec
-    int)

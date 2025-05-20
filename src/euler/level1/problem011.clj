e6d7743b77494cf638556fba8b346683c1c76837
nathandickinson32 <nathandickinson32@gmail.com>
2025-05-20 18:13:45 -0400

    some work on euler 11

--- src/euler/level1/problem011.clj
+++ src/euler/level1/problem011.clj
@@ -1,4 +1,19 @@
 (ns euler.level1.problem011)
 
-(defn euler-11 [n]
+
+
+(defn ->largest-row-product [grid]
+  (apply max (for [row (range (count grid))]
+    (apply * (get-in grid [row])))))
+
+(defn ->largest-collum-product [grid]
+  (apply max (for [row (range (dec (count grid)))
+                  collum (range (count (first grid)))]
+              (* (get-in grid [row collum])
+                 (get-in grid [(inc row) collum])))))
+
+
+(defn euler-11 [grid]
   )
+
+

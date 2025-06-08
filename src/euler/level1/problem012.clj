dacb3f262b4079bcfcb01909d58371665de5b45e
nathandickinson32 <nathandickinson32@gmail.com>
2025-06-08 11:00:19 -0400

    need to optimize somewhere in euler 12 because testing runs too long for target 500

--- src/euler/level1/problem012.clj
+++ src/euler/level1/problem012.clj
@@ -1,4 +1,16 @@
-(ns euler.level1.problem012)
+(ns euler.level1.problem012
+  (:require [euler.level1.core :as core]))
 
-(defn euler-12 [n]
-  )
+(defn count-divisors [n]
+  (count (filter #(core/divisible-by? n %) (range 1 (inc n)))))
+
+(defn ->triangle-number [n]
+  (/ (* n (inc n)) 2))
+
+(defn euler-12 [limit]
+  (loop [i 1]
+    (let [tri-num  (->triangle-number i)
+          divisors (count-divisors tri-num)]
+      (if (> divisors limit)
+        tri-num
+        (recur (inc i))))))

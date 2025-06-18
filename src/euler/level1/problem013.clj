3bef4b44edaffff019b14e9cf77c06a407d22033
nathandickinson32 <nathandickinson32@gmail.com>
2025-06-18 16:41:40 -0400

    ipm review fixes

--- src/euler/level1/problem013.clj
+++ src/euler/level1/problem013.clj
@@ -1,26 +1,18 @@
 (ns euler.level1.problem013)
 
-(defn ->substring-number [string-number]
-  (subs string-number 0 10))
-
-(defn too-small? [string-number]
-  (<= (count string-number) 10))
-
-(defn ->substring? [string-number]
-  (if (too-small? string-number)
-    string-number
-    (->substring-number string-number)))
+(defn ->first-ten-digits [string-number]
+  (->> string-number (take 10) (apply str)))
 
 (defn ->string-number [n]
   (let [string-number (str n)]
-    (->substring? string-number)))
+    (->first-ten-digits string-number)))
 
 (defn ->sum-of-numbers [numbers]
   (->> numbers
-       (flatten)
-       (reduce +)))
+       flatten
+       (apply +)))
 
 (defn euler-13 [numbers]
   (-> numbers
-      (->sum-of-numbers)
-      (->string-number)))
+      ->sum-of-numbers
+      ->string-number))
